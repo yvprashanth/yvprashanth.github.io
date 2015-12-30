e0e1968eba73031fe74463a4b8bcf02b441eb77f
Prashanth <pyerramilli@godaddy.com>
2015-12-30 13:07:48 -0700

    changes to jquery script

--- assets/js/map.js
+++ assets/js/map.js
@@ -23,7 +23,7 @@ for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map ,
-        icon: 'img/marker.png'
+        icon: 'assets/img/marker.png'
     });
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
